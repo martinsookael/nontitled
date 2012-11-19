dbdeabb4cff8e8c6722419661a09c14720ac830e
Martin Sookael <martinsookael@gmail.com>
2012-11-19 17:22:38 +0200

    refactore

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+
+
+	// move the clouds
+	// declare variables
+	var scrollSpeed = 110;       // Speed in milliseconds
+	var step = 1;               // How many pixels to move per step
+	var current = 0;            // The current pixel row
+	var imageHeight = 2400;     // Background image height
+	var headerHeight = 1200;     // How tall the header is.
+	
+	//The pixel row where to start a new loop
+	var restartPosition = -(imageHeight - headerHeight);
+	
+	function scrollBg(){
+		
+		//Go to next pixel row.
+		current -= step;
+		
+		//If at the end of the image, then go to the top.
+		if (current == restartPosition){
+				current = 0;
+		}
+		
+		//Set the CSS of the header.
+		$('#clouds').css("background-position",""+current+"px 0");
+		
+	}
+	
+	//Calls the scrolling function repeatedly
+	var init = setInterval("scrollBg()", scrollSpeed);
+
+
+
+	// Play music
+	 $(document).ready(function(){
+		$("#jquery_jplayer_1").jPlayer({
+		 ready: function () {
+			$(this).jPlayer("setMedia", {
+			 mp3: "img/air_lastejupp.mp3"
+			 //ogg: "img/air_o.ogg"
+	
+			}).jPlayer("play");
+		 },
+		 supplied: "mp3",
+		 //supplied: "ogg",
+		 loop: "true"
+		});
+	 });
+	 
+		// FB like button
+		(function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) return;
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=447481895288765";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
